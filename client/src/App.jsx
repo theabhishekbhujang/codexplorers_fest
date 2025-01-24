5a789f22bb95b29564c06686ce19ca6330724f54
Abhishek Bhujang <abhishekbhujang2@gmail.com>
2025-01-25 00:49:30 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import HeroSection from './components/HeroSection';
 import EventsGrid from './components/EventsGrid';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
